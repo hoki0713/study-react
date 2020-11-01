0bc0f94f3936a297f7c84d0049b9ec6b8a36317f
Hojeong <hjkim@firecop365.com>
2020-11-01 20:23:18 +0900

    Study React and CSS modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { id: 'a', name: "Max", age: 28 },
-      { id: 'b', name: "Manu", age: 29 },
-      { id: 'c', name: "Stephanie", age: 26 }
+      { id: "a", name: "Max", age: 28 },
+      { id: "b", name: "Manu", age: 29 },
+      { id: "c", name: "Stephanie", age: 26 },
     ],
-    otherState: 'some other value',
+    otherState: "some other value",
     showPersons: false,
-  }
+  };
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(person => {
+    const personIndex = this.state.persons.findIndex((person) => {
       return person.id === id;
     });
 
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     };
 
     // const person = Object.assign({}, this.state.persons[personIndex]);
@@ -30,53 +30,78 @@ class App extends Component {
     persons[personIndex] = person;
 
     this.setState({ persons: persons });
-  }
+  };
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
-  }
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
-  }
+  };
 
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
+    // const style = {
+    //   backgroundColor: "green",
+    //   color: "white",
+    //   font: "inherit",
+    //   border: "1px solid blue",
+    //   padding: "8px",
+    //   cursor: "pointer",
+    //   ":hover": {
+    //     backgroundColor: "lightgreen",
+    //     color: "black",
+    //   },
+    // };
 
     let persons = null;
 
     if (this.state.showPersons) {
       persons = (
-        <div >
+        <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              key={person.id}
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return (
+              <Person
+                key={person.id}
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
         </div>
-      )
+      );
+      // style.backgroundColor = "red";
+      // style[":hover"] = {
+      //   backgroundColor: "salmon",
+      //   color: "black",
+      // };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          className="button"
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
+          Toggle Persons
+        </button>
         {persons}
       </div>
     );
--- react-complete-guide/src/ValidationComponent/ValidationComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const validationComponent = ({ txtLength }) => {
-    return txtLength >= 5 ? "Text long enough" : "Text too short";
-}
-
-export default validationComponent;
